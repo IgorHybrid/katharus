9d83416eab5f3a1d2cfba28dd3d9e82ab2807014
IgorHybrid <igor@mihybrid.com>
2018-04-15 16:31:17 +0200

    Connection to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = require('./routes/routes.js')
 var path = require('path');
-
+var bodyParser = require('body-parser');
 var app = express();
+var mongoose = require('mongoose');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
+
+mongoose.connect('mongodb://<connect>:<PRrggL6t9FQanp4d>@ds139459.mlab.com:39459/katharus');
 
 app.use('/', router);
 
