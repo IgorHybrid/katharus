56f4047cdbe4582837a2649e048ffab0512d258c
IgorHybrid <igor@mihybrid.com>
2018-04-15 20:13:54 +0200

    Changed viem engine from ejs to pug.

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var mongoose = require('mongoose');
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json({limit: '50mb'}));
