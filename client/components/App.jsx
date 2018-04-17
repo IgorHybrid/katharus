5ab16d1e2447baab6074a0cbb8e16f2d231c69d9
IgorHybrid <igor@mihybrid.com>
2018-04-17 16:29:42 +0200

    Router for all endpoints
    Created routes for all endpoints.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-import Menu from './Menu.jsx';
+//components
+import Menu   from "./Menu.jsx";
+import Home   from "./Home.jsx";
+import About  from "./About.jsx";
+import Login  from "./Login.jsx";
+import Signup from "./Signup.jsx";
 
 class App extends React.Component {
   render() {
     return (
-      <Menu/>
+      <div>
+        <Menu />
+        <Router>
+          <div>
+            <Route exact path = "/"        component = { Home }   />
+            <Route       path = "/about"   component = { About }  />
+            <Route       path = "/log-in"  component = { Login }  />
+            <Route       path = "/sign-up" component = { Signup } />
+          </div>
+        </Router>
+      </div>
     );
   }
 }
