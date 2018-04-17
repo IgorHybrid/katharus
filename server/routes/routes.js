42345f8769a4c80921e458f824c9f979c750b298
IgorHybrid <igor@mihybrid.com>
2018-04-17 16:28:55 +0200

    Endpoints created
    Created endpoints for GET /about, /log-in and /sign-up.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,4 +5,17 @@ router.get('/', function(req, res){
   res.render('index')
 });
 
+router.get('/about', function(req, res){
+  res.render('index')
+});
+
+router.get('/sign-up', function(req, res){
+  res.render('index')
+});
+
+router.get('/log-in', function(req, res){
+  res.render('index')
+});
+
+
 module.exports = router;
