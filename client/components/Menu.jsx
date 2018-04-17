f594be3612ad862269656aaf1984577e6a3e175a
IgorHybrid <igor@mihybrid.com>
2018-04-17 16:33:36 +0200

    Made Menu operational
    Now menu is fully operational.

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 
 export default class Menu extends React.Component {
   render() {
@@ -14,19 +13,19 @@ export default class Menu extends React.Component {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav>
-            <NavItem eventKey={1} href="#">
+            <NavItem eventKey={1} href = '/'>
               Home
             </NavItem>
-            <NavItem eventKey={2} href="#">
+            <NavItem eventKey={2} href = '/about'>
               About Us
             </NavItem>
           </Nav>
           <Nav pullRight>
-            <NavItem eventKey={1} href="#">
+            <NavItem eventKey={1} href = '/log-in'>
               Log In
             </NavItem>
-            <NavItem eventKey={2} href="#">
-              Sing Up
+            <NavItem eventKey={2} href = '/sign-up'>
+              Sign Up
             </NavItem>
           </Nav>
         </Navbar.Collapse>
