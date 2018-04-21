06c43b7b9aed7294d0db30ffcbfeb432a8dee744
IgorHybrid <igor@mihybrid.com>
2018-04-21 10:16:49 +0200

    Jumbotron added
    Jumbotron  with company slogan added in the main page.

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Jumbotron, Grid, Row, Col } from 'react-bootstrap';
 
 export default class Home extends React.Component{
   render(){
     return(
-      <h1>HOME</h1>
+      <Grid>
+        <Row>
+          <Col xs = { 12 }>
+            <Jumbotron>
+              <h1>Katharus</h1>
+              <h2><em>"Travel with us, travel with Katharus"</em></h2>
+              <p>Your favourite website for student exchange</p>
+            </Jumbotron>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 };
