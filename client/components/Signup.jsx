2680502566a3a21ccdceb16f6bf73a27863092b5
IgorHybrid <igor@mihybrid.com>
2018-04-21 11:34:03 +0200

    Sign up form added
    Sign up form added in /sign-up.

--- client/components/Signup.jsx
+++ client/components/Signup.jsx
@@ -1,9 +1,75 @@
 import React from 'react';
+import { FormGroup, ControlLabel ,FormControl, HelpBlock } from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-bootstrap';
+import { Button, ButtonToolbar } from 'react-bootstrap';
+
+import '../css/Signup.css';
 
 export default class Signup extends React.Component{
   render(){
     return(
-      <h1>SIGN UP</h1>
+      <Grid>
+        <form>
+          <FormGroup>
+            <Row>
+              <Col xs = { 0 }  md = { 3 }></Col>
+              <Col xs = { 12 } md = { 6 }>
+                <ControlLabel>Username</ControlLabel>
+                <FormControl
+                  id = "username"
+                  type = "text"
+                  placeholder = "Enter Username"
+                />
+              </Col>
+              <Col xs = { 0 }  md = { 3 }></Col>
+            </Row>
+            <Row>
+              <Col xs = { 0 }  md = { 3 }></Col>
+              <Col xs = { 12 } md = { 6 }>
+                <ControlLabel>Email</ControlLabel>
+                <FormControl
+                  id = "email"
+                  type = "email"
+                  placeholder = "Enter a valid email"
+                />
+              </Col>
+              <Col xs = { 0 }  md = { 3 }></Col>
+            </Row>
+            <Row>
+              <Col xs = { 0 }  md = { 3 }></Col>
+              <Col xs = { 12 } md = { 6 }>
+                <ControlLabel>Password</ControlLabel>
+                <FormControl
+                  id = "password"
+                  type = "password"
+                />
+              </Col>
+              <Col xs = { 0 }  md = { 3 }></Col>
+            </Row>
+            <Row>
+              <Col xs = { 0 }  md = { 3 }></Col>
+              <Col xs = { 12 } md = { 6 }>
+                <ControlLabel>Confirm Password</ControlLabel>
+                <FormControl
+                  id = "confirmPassword"
+                  type = "password"
+                />
+              </Col>
+              <Col xs = { 0 }  md = { 3 }></Col>
+            </Row>
+            <Row>
+              <Col xs = { 0 }  md = { 3 }></Col>
+              <Col xs = { 12 } md = { 6 }>
+                <ButtonToolbar id = "buttontoolbar">
+                  <Button bsStyle = "primary">Submit</Button>
+                  <Button bsStyle = "danger" >Clean</Button>
+                </ButtonToolbar>
+              </Col>
+              <Col xs = { 0 }  md = { 3 }></Col>
+            </Row>
+          </FormGroup>
+        </form>
+      </Grid>
     );
   }
 };
