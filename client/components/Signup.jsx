f6276127612f28f2505e178f6cc28eb8aa01a394
IgorHybrid <igor@mihybrid.com>
2018-04-23 10:54:44 +0200

    Form for sign up created
    When the user is correctly created, it is redirected to log in page.

--- client/components/Signup.jsx
+++ client/components/Signup.jsx
@@ -1,23 +1,71 @@
 import React from 'react';
+import axios from 'axios';
 import { FormGroup, ControlLabel ,FormControl, HelpBlock } from 'react-bootstrap';
 import { Grid, Row, Col } from 'react-bootstrap';
 import { Button, ButtonToolbar } from 'react-bootstrap';
+import { Redirect } from 'react-router'
 
 import '../css/Signup.css';
 
 export default class Signup extends React.Component{
   constructor(props) {
     super(props);
+    this.state = {
+      redirect: false,
+      username: null,
+      password: null,
+      email: null,
+      confirmPassword: null,
+    }
     this.signForm = React.createRef();
+    this.errorMsg = React.createRef();
   }
+
   cleanForm(){
     this.signForm.current.reset();
   }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.id;
+
+    this.setState({
+        [name]: value
+      });
+  }
+
+  handleSubmit(e){
+
+    e.preventDefault();
+
+    axios.post('/sign-up', {
+      username: this.state.username,
+      email: this.state.email,
+      password: this.state.password,
+      confirmPassword: this.state.confirmPassword,
+    })
+    .then(response => {
+      this.setState({ redirect: true });
+    });
+  }
+
   render(){
+    if (this.state.redirect) {
+      return <Redirect to='/log-in'/>;
+    }
+
     return(
       <Grid>
-        <form id = "signForm" ref = {this.signForm}>
-          <FormGroup>
+        <Row>
+          <Col xs = { 0 }  md = { 3 }></Col>
+          <Col xs = { 12 } md = { 6 }>
+            <div ref = { this.errorMsg }></div>
+          </Col>
+          <Col xs = { 0 }  md = { 3 }></Col>
+        </Row>
+        <form id = "signForm" ref = { this.signForm } onSubmit = { this.handleSubmit.bind(this) }>
+          <FormGroup role = 'form'>
             <Row>
               <Col xs = { 0 }  md = { 3 }></Col>
               <Col xs = { 12 } md = { 6 }>
@@ -26,6 +74,7 @@ export default class Signup extends React.Component{
                   id = "username"
                   type = "text"
                   placeholder = "Enter Username"
+                  onChange = { this.handleInputChange.bind(this) }
                 />
               </Col>
               <Col xs = { 0 }  md = { 3 }></Col>
@@ -36,8 +85,9 @@ export default class Signup extends React.Component{
                 <ControlLabel>Email</ControlLabel>
                 <FormControl
                   id = "email"
-                  type = "email"
+                  type = "text"
                   placeholder = "Enter a valid email"
+                  onChange = { this.handleInputChange.bind(this) }
                 />
               </Col>
               <Col xs = { 0 }  md = { 3 }></Col>
@@ -49,7 +99,9 @@ export default class Signup extends React.Component{
                 <FormControl
                   id = "password"
                   type = "password"
+                  onChange = { this.handleInputChange.bind(this) }
                 />
+                <HelpBlock>Password should be al last 8 chars length and contain one number.</HelpBlock>
               </Col>
               <Col xs = { 0 }  md = { 3 }></Col>
             </Row>
@@ -60,6 +112,7 @@ export default class Signup extends React.Component{
                 <FormControl
                   id = "confirmPassword"
                   type = "password"
+                  onChange = { this.handleInputChange.bind(this) }
                 />
               </Col>
               <Col xs = { 0 }  md = { 3 }></Col>
@@ -68,7 +121,7 @@ export default class Signup extends React.Component{
               <Col xs = { 0 }  md = { 3 }></Col>
               <Col xs = { 12 } md = { 6 }>
                 <ButtonToolbar id = "buttontoolbar">
-                  <Button bsStyle = "primary">Submit</Button>
+                  <Button bsStyle = "primary" type = "submit">Submit</Button>
                   <Button bsStyle = "danger" onClick = { this.cleanForm.bind(this) }>Clean</Button>
                 </ButtonToolbar>
               </Col>
