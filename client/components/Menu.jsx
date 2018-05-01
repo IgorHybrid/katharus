de091ed7202ce75dd2210df45a846d201c0e06a6
IgorHybrid <igor@mihybrid.com>
2018-05-01 19:14:32 +0200

    Menu updated

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import Cookies from 'universal-cookie';
+
+
+//Login and logout components
+import MenuLogin from './MenuLogin.jsx';
+import MenuUser  from './MenuUser.jsx';
 
 export default class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+    const cookies = new Cookies();
+    console.log(cookies.get('user'));
+    this.state = {
+      user: cookies.get('user') || null
+    }
+  }
+
   render() {
+    var { user } = this.state;
+    console.log(user);
+    var logMenu;
+    if(user){
+      logMenu = <MenuUser user = { user }/>;
+    }else{
+      logMenu = <MenuLogin />;
+    }
     return (
       <Navbar inverse collapseOnSelect>
         <Navbar.Header>
@@ -20,14 +43,7 @@ export default class Menu extends React.Component {
               About Us
             </NavItem>
           </Nav>
-          <Nav pullRight>
-            <NavItem eventKey={1} href = '/log-in'>
-              Log In
-            </NavItem>
-            <NavItem eventKey={2} href = '/sign-up'>
-              Sign Up
-            </NavItem>
-          </Nav>
+          {logMenu}
         </Navbar.Collapse>
       </Navbar>
     );
