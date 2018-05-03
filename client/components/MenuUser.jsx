abf2cec0e2381e551a54bd769146f39320fa4775
IgorHybrid <igor@mihybrid.com>
2018-05-03 17:36:16 +0200

    Added user logout

--- client/components/MenuUser.jsx
+++ client/components/MenuUser.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { Nav, NavDropdown, MenuItem } from 'react-bootstrap';
 
 export default class MenuLogin extends React.Component {
@@ -6,13 +7,14 @@ export default class MenuLogin extends React.Component {
     super(props);
     this.navtitle = "Hi, " + this.props.user.username;
   }
+
   render(){
     return (
       <Nav pullRight>
         <NavDropdown title = { this.navtitle }  eventKey = { 1 } id = "dropdown">
           <MenuItem eventKey = { 1.1 }> Profile </MenuItem>
           <MenuItem divider />
-          <MenuItem eventKey = { 1.2 }> Logout </MenuItem>
+          <MenuItem eventKey = { 1.2 } href = "/logout"> Logout </MenuItem>
         </NavDropdown>
       </Nav>
     );
