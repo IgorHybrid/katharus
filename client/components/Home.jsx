557d56190367d7b772e96de26082ca3f0571e337
IgorHybrid <igor@mihybrid.com>
2018-05-21 20:53:58 +0200

    Added home style

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Jumbotron, Grid, Row, Col } from 'react-bootstrap';
 
+//Styles CSS
+import  "../css/Home.css";
+
 export default class Home extends React.Component{
   render(){
     return(
       <Grid>
         <Row>
           <Col xs = { 12 }>
-            <Jumbotron>
+            <Jumbotron id="jumbo">
               <h1>Katharus</h1>
               <h2><em>"Travel with us, travel with Katharus"</em></h2>
               <p>Your favourite website for student exchange</p>
